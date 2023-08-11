14dc63484c98afb44f91bb6f08981370f084c861
Razi <syedru@mcmaster.ca>
2023-08-10 23:05:28 -0700

    move files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+export default function Navbar() {
   return (
     <Disclosure as="nav" className="bg-dark-green">
       {({ open }) => (
